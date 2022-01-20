b2070c13bf99d87c4501e530fa0e96a92b3fce19
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-01-20 09:29:49 +0300

    ADDing a little bit of bascket page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// ======= Modal login ========
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("myModal");  // Get the modal
+const btn = document.getElementById("myBtn");      // Get the button that opens the modal
+const span = document.getElementsByClassName("close")[0];    // Get the <span> element that closes the modal
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -23,3 +20,33 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// ======= Offer Down Counter =======
+
+const dateDownCounter = new Date("Jan 30, 2022 23:59:59").getTime();
+
+// update the count every 1000ms (1s)
+const updatedTime = setInterval(function(){
+  const now = new Date().getTime();
+  const interval = dateDownCounter - now;
+
+  const days = Math.floor(interval / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((interval % (1000 * 60)) / 1000);
+
+  document.getElementById("offer-counting1").innerHTML = days + "ي " + hours + "س " + minutes + "د " + seconds + "ث ";
+  document.getElementById("offer-counting2").innerHTML = days + "ي " + hours + "س " + minutes + "د " + seconds + "ث ";
+  document.getElementById("offer-counting3").innerHTML = days + "ي " + hours + "س " + minutes + "د " + seconds + "ث ";
+  document.getElementById("offer-counting4").innerHTML = days + "ي " + hours + "س " + minutes + "د " + seconds + "ث ";
+  document.getElementById("offer-counting5").innerHTML = days + "ي " + hours + "س " + minutes + "د " + seconds + "ث ";
+
+  if (interval < 0) {
+    clearInterval(x);
+    document.getElementById("offer-counting1").innerHTML = "time Expoired";
+    document.getElementById("offer-counting2").innerHTML = "time Expoired";
+    document.getElementById("offer-counting3").innerHTML = "time Expoired";
+    document.getElementById("offer-counting4").innerHTML = "time Expoired";
+    document.getElementById("offer-counting5").innerHTML = "time Expoired";
+  }
+}, 1000);
