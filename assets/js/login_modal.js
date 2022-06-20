00f98c508013bfcef98967de69f16ec3595042f4
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-06-20 22:01:26 +0300

    Implement:code of (When the user clicks anywhere outside of the modal, close it) in login_modal.js

--- assets/js/login_modal.js
+++ assets/js/login_modal.js
@@ -9,3 +9,12 @@ login.addEventListener('click', ()=>{
 close__login.addEventListener('click', ()=>{
     login__modal.style.display = "none";
 });
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function(event) {
+    var modal = document.getElementById('login__modal');
+
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
