58ec5e27f6cdc492272b1ad6aa605e7f7ce40601
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-06-21 22:20:38 +0300

    Add: register modal events

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 // ########################## MODAL | LOGIN & REGISTER ##########################
 
+// ========== Login modal
 const login         = document.getElementById("login");
 const login__modal  = document.getElementById("login__modal");
 const close__login  = document.getElementById("close__login");
@@ -12,12 +13,35 @@ close__login.addEventListener('click', ()=>{
     login__modal.style.display = "none";
 });
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of login modal, close it
 window.addEventListener('click', function(event) {
     var log_modal = document.getElementById('login__modal');
 
     if (event.target == log_modal) {
         log_modal.style.display = "none";
+    }
+});
+
+// ========== Register modal
+const register          = document.getElementById("register");
+const register__modal   = document.getElementById("register__modal");
+const close__register   = document.getElementById("close__register");
+
+register.addEventListener('click', ()=>{
+    login__modal.style.display = "none";
+    register__modal.style.display = "block";
+});
+
+close__register.addEventListener('click', ()=>{
+    register__modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of register modal, close it
+window.addEventListener('click', function(event) {
+    var reg_modal = document.getElementById('register__modal');
+
+    if (event.target == reg_modal) {
+        reg_modal.style.display = "none";
     }
 });
 
